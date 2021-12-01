006f8ced48bf9601dd902b288d52c8f10882352e
Bulat Rakhimov <bulich121@mail.ru>
2021-12-02 00:03:21 +0300

    Create LivingWay.cs

---
+++ hw2/LivingWay.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.hw2_LivingWay_
+{
+    class LivingWay
+    {
+        double cost;
+        int minArenda;
+        int maxArenda;
+        bool isAdvanceNeed;
+
+        void CheckIn() { }
+        void CheckOut() { }
+
+    }
+    public enum RoomType
+    {
+        single,
+        twin,
+        family
+    }
+
+    public enum FoodSupply
+    {
+        breakfast,
+        fullDay
+    }
+
+    class Hotel : LivingWay
+    {
+        public int roomNumber;
+        public RoomType roomType;
+        public FoodSupply foodSupply;
+    }
+
+    public enum TypeOfHousing
+    {
+        room,
+        flat,
+        house
+    }
+
+    class PrivateHouse : LivingWay
+    {
+        public TypeOfHousing typeOfHousing;
+        public bool isCleaningIncluded;
+        public bool isSecondKeysIncluded;
+        public bool isInternetIncluded;
+    }
+}
