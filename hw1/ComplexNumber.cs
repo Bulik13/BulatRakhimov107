deaedc413ba49f2eef1c8ba1797c85853f422c99
Bulat Rakhimov <bulich121@mail.ru>
2021-12-01 23:57:55 +0300

    hw

---
+++ hw1/ComplexNumber.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.hw1
+{
+    public class ComplexNumber
+    {
+        public int RealPart { get; set; }
+        public int Image { get; set; }
+        public ComplexNumber(int r, int i)
+        {
+            RealPart = r;
+            Image = i;
+        }
+        public ComplexNumber()
+        {
+            RealPart = 0;
+            Image = 0;
+        }
+        public ComplexNumber Add(ComplexNumber n)
+        {
+            ComplexNumber number = new ComplexNumber(RealPart + n.RealPart, n.Image + Image);
+            return number;
+        }
+        public void Add_toYourself(ComplexNumber c)
+        {
+            RealPart += c.RealPart;
+            Image += c.Image;
+        }
+        public ComplexNumber Sub(ComplexNumber c)
+        {
+            ComplexNumber number = new ComplexNumber(RealPart - c.RealPart, Image - c.Image);
+            return number;
+        }
+        public void Sub_toYourself(ComplexNumber c)
+        {
+            RealPart -= c.RealPart;
+            Image -= c.Image;
+        }
+        public ComplexNumber Mult(ComplexNumber c)
+        {
+            ComplexNumber number = new ComplexNumber(RealPart * c.RealPart - Image * c.Image, RealPart * c.Image + c.RealPart * Image);
+            return number;
+        }
+        public void Mult_toYourself(ComplexNumber c)
+        {
+            RealPart = RealPart * c.RealPart - Image * c.Image;
+            Image = RealPart * c.Image + c.RealPart * Image;
+        }
+        public ComplexNumber Div(ComplexNumber c)
+        {
+            ComplexNumber number = new ComplexNumber((RealPart * c.RealPart - Image * c.Image) / (c.RealPart * c.RealPart + c.Image * c.Image), (RealPart * c.Image + c.RealPart * Image) / (c.RealPart * c.RealPart + c.Image * c.Image));
+            return number;
+        }
+        public void Div_toYourself(ComplexNumber c)
+        {
+            RealPart = (RealPart * c.RealPart + Image * c.Image)/(c.RealPart * c.RealPart + c.Image * c.Image);
+            Image = (RealPart * c.Image - c.RealPart * Image)/(c.RealPart * c.RealPart + c.Image * c.Image);
+        }
+        public bool Eqauls(ComplexNumber n)
+        {
+            bool result = (RealPart == n.RealPart && Image == n.Image);
+            return result;
+        }
+        public double Length()
+        {
+            return Math.Sqrt(RealPart * RealPart + Image * Image);
+        }
+        public double Arg()
+        {
+            double z;
+            if (RealPart > 0)
+                z = Math.Atan(Image / RealPart);
+            else if (RealPart < 0 && Image >= 0)
+                z = Math.PI + Math.Atan(Image / RealPart);
+            else if (RealPart < 0 && Image < 0)
+                z = -Math.PI + Math.Atan(Image / RealPart);
+            else if (RealPart == 0 && Image > 0)
+                z = Math.PI / 2;
+            else
+                z = -Math.PI / 2;
+            return z;
+        }
+        public string TooString()
+        {
+            return $"{RealPart}+{Image}*i";
+        } 
+    }
+}
---
+++ hw1/Matrix2x2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.hw1
+{
+    public class Matrix2x2
+    {
+        //комплексная матрица и матрица 2х2 по сути одно и тоже, я в одной сделал)) если поменять на int или double то получится обычная :)
+        private ComplexNumber[,] arr = new ComplexNumber[2, 2];
+        public Matrix2x2()
+        {
+            var b = new ComplexNumber();
+            arr = new ComplexNumber[2, 2] { { b, b }, { b, b } };
+        }
+        public ComplexNumber this[int i, int j]
+        {
+            get
+            {
+                return arr[i, j];
+            }
+            set
+            {
+                arr[i, j] = value;
+            }
+        }
+        public Matrix2x2(ComplexNumber a)
+        {
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 2; j++)
+                    arr[i, j] = a;
+        }
+        public Matrix2x2(ComplexNumber[][] a)
+        {
+            for (int i = 0; i < 2; i++)
+                arr[0, i] = a[0][i];
+            for (int i = 0; i < 2; i++)
+                arr[1, i] = a[1][i];
+        }
+        public Matrix2x2(ComplexNumber a, ComplexNumber b, ComplexNumber c, ComplexNumber d)
+        {
+            arr[0, 0] = a;
+            arr[0, 1] = b;
+            arr[1, 0] = c;
+            arr[1, 1] = d;
+        }
+        public Matrix2x2 Add(Matrix2x2 a)
+        {
+            Matrix2x2 z = new Matrix2x2();
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 2; j++)
+                    z[i, j] = a.arr[i, j].Add(arr[i, j]);
+            return z;
+        }
+        public Matrix2x2 Mult(Matrix2x2 a)
+        {
+            Matrix2x2 arr1 = new Matrix2x2();
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 2; j++)
+                    for (int k = 0; k < 2; k++)
+                        arr1[i, j] = arr1[i, j].Add(arr[i, k].Mult(a[k, j]));
+            return arr1;
+        }
+        public ComplexNumber det()
+        {
+            return arr[0, 0].Mult(arr[1, 1]).Sub(arr[0, 1].Mult(arr[1, 0]));
+        }
+        public void transport()
+        {
+            ComplexNumber z;
+            z = arr[0, 1];
+            arr[0, 1] = arr[1, 0];
+            arr[1, 0] = z;
+        }
+        public void Print()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                    Console.Write(arr[i, j].TooString() + "\t");
+                Console.WriteLine();
+            }
+        }
+    }
+}
