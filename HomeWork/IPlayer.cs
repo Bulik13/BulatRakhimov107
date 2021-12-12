377c6db11c704306f69dc167f655437cdf4753dc
Bulat Rakhimov <bulich121@mail.ru>
2021-12-12 12:28:41 +0300

    .

---
+++ HomeWork/IPlayer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    public interface IPlayer
+    {
+        public void Play();
+    }
+    public class FootballPlayer : IPlayer
+    {
+        public string FIO;
+        public int Age
+        { get; set; }
+        public FootballPlayer(string fio, int age)
+        {
+            FIO = fio;
+            Age = age;
+        }
+        public void Play()
+        {
+            Console.WriteLine($"{FIO} играет на поле");
+        }
+    }
+    public class Musician : IPlayer
+    {
+        public string FIO;
+        public Instrument Instrument;
+        public Musician(string fio, Instrument i)
+        {
+            FIO = fio;
+            Instrument = i;
+        }
+        public void Play()
+        {
+            Console.WriteLine($"{FIO} играет на {Instrument}");
+        }
+    }
+    public enum Instrument
+    {
+        Piano,
+        Violin,
+        Guitar,
+        Drum
+    }
+    public class PlayerManager
+    {
+        public void Test(IPlayer[] x)
+        {
+            foreach (var player in x)
+            {
+                if (player is Musician)
+                {
+                    if (((Musician)player).Instrument == Instrument.Violin)
+                        player.Play();
+                }
+                if (player is FootballPlayer)
+                {
+                    if (((FootballPlayer)player).Age < 20)
+                        player.Play();
+                }
+            }
+        }
+    }
+}
---
+++ IPlayer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public interface IPlayer
+    {
+        public void Play();
+    }
+
+    public class FootballPlayer : IPlayer
+    {
+        public string FIO;
+        public int Age
+        {
+            get { return Age; }
+            set
+            {
+                if (value < 0)
+                    throw new Exception();
+                else
+                    Age = value;
+            }
+        }
+        public void Play()
+        {
+            Console.WriteLine($"{FIO} играет на поле");
+        }
+    }
+    public class Musician : IPlayer
+    {
+        public string FIO;
+        public Instrument Instrument;
+        public void Play()
+        {
+            Console.WriteLine($"{FIO} играет на {Instrument}");
+        }
+    }
+    public enum Instrument
+    {
+        Piano,
+        Violin,
+        Guitar,
+        Drum
+    }
+    public class PlayerManager
+    {
+        public void Test(IPlayer[] x)
+
+    }
+}
